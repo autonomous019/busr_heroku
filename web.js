0b421890cc60dbfc2292a8b6614108b6544b7bc0
mike mccarron <m.j.h.mccarron@gmail.com>
2015-08-02 10:25:07 -0700

    putting redis vars in web.js

--- web.js
+++ web.js
@@ -27,7 +27,13 @@ app.use(express.static('public'));
 //SESSIONS SESSIONS SESSIONS 
 //var everyauth = require('everyauth');
 
-
+//heroku code requirements
+var redis = require('redis');
+var url = require('url');
+var redisURL = url.parse(process.env.REDISCLOUD_URL);
+var client = redis.createClient(redisURL.port, redisURL.hostname, {no_ready_check: true});
+client.auth(redisURL.auth.split(":")[1]);
+// end heroku
 
 
 /********** MODULE SCRIPTS ****************/
